f47f8977c4dad1d8003132de4094abc641cf933f
CollinHm <collinhermans7@gmail.com>
2023-10-12 10:43:40 +0200

    Final Pong basic
    we added a score system and made it so the paddles can't go through the walls

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using TMPro;
 
 public class ball : MonoBehaviour
 {
     public float Xposition = 0f;
     public float Yposition = 0f;
-    public float xSpeed;
-    public float ySpeed;
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+    public TMP_Text scoreField;
+    private int leftScore = 0;
+    private int rightScore = 0;
+    public int topScore = 10;
+
+
+   private void resetBall(string leftOrRight)
+    {
+        Xposition = 0f;
+        Yposition = 0f;
+        scoreField.text = leftScore + " - " + rightScore;
+        if (leftOrRight == "left") 
+        {
+            xSpeed = -3f;
+            ySpeed =  3f;
+        }
+        else if (leftOrRight == "right")
+        {
+            xSpeed = 3f;
+            ySpeed = -3f;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
 
        // transform.position = new Vector3(Xposition, Yposition, 0);
-        xSpeed = 3f;
-        ySpeed = 1f;
+       
+        xSpeed = 7f;
+        ySpeed = 3f;
     }
 
     // Update is called once per frame
@@ -26,27 +50,49 @@ public class ball : MonoBehaviour
         Xposition += xSpeed * Time.deltaTime;
         Yposition += ySpeed * Time.deltaTime;
         transform.position = new Vector3(Xposition, Yposition, 0);
-
+        if(leftScore >= topScore)
+        {
+            scoreField.text = "Left Player has won!";
+            xSpeed = 0;
+            ySpeed = 0;
+            Xposition = 0f;
+            Yposition = 0f;
+        }
+        else if (rightScore >= topScore)
+        {
+            scoreField.text = "Right Player has won!";
+            xSpeed = 0;
+            ySpeed = 0;
+            Xposition = 0f;
+            Yposition = 0f;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("horizontalWall"))
         {
-            ySpeed = ySpeed * -1;
+            ySpeed = ySpeed * -1f;
         }
-        if (collision.gameObject.CompareTag("verticalWall"))
+       else if (collision.gameObject.CompareTag("leftWall"))
         {
-            xSpeed = xSpeed * -1;
+            
+            rightScore++;
+            resetBall("left");
+            
         }
-        if (collision.gameObject.CompareTag("rightPaddle"))
+        else if (collision.gameObject.CompareTag("rightWall"))
         {
-            xSpeed = xSpeed * -1;
+            
+            leftScore++;
+            resetBall("right");    
+            
         }
-        if (collision.gameObject.CompareTag("leftPaddle"))
+        else if (collision.gameObject.CompareTag("player"))
         {
-            xSpeed = xSpeed * -1;
+            xSpeed = xSpeed * -1.1f;
         }
+      
        
     }
 
--- Assets/Scripts/inputPaddle.cs
+++ Assets/Scripts/inputPaddle.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class inputPaddle : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 24f;
     public string leftOrRight;
  
    
@@ -20,12 +20,12 @@ public class inputPaddle : MonoBehaviour
     {
         if (leftOrRight == "left")
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && transform.position.y <= 3.9f)
             {
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
                 Debug.Log("Yes W is pressed down.");
             }
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S) && transform.position.y >= -3.9f)
             {
                 transform.Translate(Vector3.down * speed * Time.deltaTime);
                 Debug.Log("Yes S is pressed down.");
@@ -33,12 +33,12 @@ public class inputPaddle : MonoBehaviour
         }
         else if (leftOrRight == "right")
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.UpArrow) && transform.position.y <= 3.9f)
             {
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
                 Debug.Log("Yes upArrow is pressed down.");
             }
-            else if (Input.GetKey(KeyCode.DownArrow))
+            else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y >= -3.9f)
             {
                 transform.Translate(Vector3.down * speed * Time.deltaTime);
                 Debug.Log("Yes downArrow is pressed down.");
