4d367ec2fa11a69ac2e6023bd568a8558955336e
CollinHm <collinhermans7@gmail.com>
2023-10-30 10:35:05 +0100

    Pong updates
    hehehe

---
+++ Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+
+
+    public Button Singleplayer_Button;
+    public Button Multiplayer_Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Singleplayer_pongOnePlayer()
+    {
+
+        SceneManager.LoadScene("pongOnePlayer");
+    }
+
+    public void Multiplayer_pongTwoPlayer()
+    {
+        SceneManager.LoadScene("pongTwoPlayer");
+    }
+
+
+
+}
